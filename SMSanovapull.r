fa4184e3f090136ee9353c0fd7e49c85c58180b4
awdesch <melzeraw@hu-berlin.de>
2015-08-11 15:57:39 +0200

    Update SMSanovapull.r

--- SMSanovapull.r
+++ SMSanovapull.r
@@ -74,8 +74,8 @@ rm(list=ls(all=TRUE))
 graphics.off()
 
 y   = c(9,11,10,12,7,11,12,10,11,13,10,15,11,15,15,13,7,15,13,10,18,14,17,9,14,17,16,14,17,15)
-x   = factor(rep(1:3,each=10))
-lm1 = lm(y~x)
+x   = factor(rep(1:3,each=10))   # factor variable for 3 strategies and 30 observations
+lm1 = lm(y~x)                    # linear model
 
-anova(lm1)
-summary(lm1)
+anova(lm1)                       # anova table
+summary(lm1)                     # regression summary
