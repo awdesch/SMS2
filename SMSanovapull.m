a8d85a5d1d5c2173f87f398d0c1dd3c3b556dca8
awdesch <melzeraw@hu-berlin.de>
2015-08-11 16:05:18 +0200

    Update SMSanovapull.m
    header, comments

--- SMSanovapull.m
+++ SMSanovapull.m
@@ -16,6 +16,17 @@
 %              y1 = (9, 11, 10, 12, 7, 11, 12, 10, 11, 13)',
 %              y2 = (10, 15, 11, 15, 15, 13, 7, 15, 13, 10)', and
 %              y3 = (18, 14, 17, 9, 14, 17, 16, 14, 17, 15)', respectively.
+%              A null hypothesis of equality of means of the three groups
+%              is tested. The standard approach of using ANOVA leads to
+%              an F-test. The alternative proposition is to use a
+%              linear factor model using the strategies as regression
+%              variables to build curve that corresponds to the alternative
+%              hypothesis of the ANOVA with three horizontal lines,
+%              each for one strategy. The F-test of testing equality of
+%              the coefficients to zero corresponds to the null of testing
+%              the marketing strategies having no effect.
+%              The output shows rejection of the null hypothesis.
+%              For more information see Exercise 3.20.
 % ----------------------------------------------------------------------
 % Inputs:      None.
 % ----------------------------------------------------------------------
@@ -143,9 +154,9 @@ close all
 clc
 
 y   = [9,11,10,12,7,11,12,10,11,13,10,15,11,15,15,13,7,15,13,10,18,14,17,9,14,17,16,14,17,15]';
-x   = [1*ones(10,1);2*ones(10,1);3*ones(10,1)];
-X   = [ones(30,1),x];
-[b,bint,r,rint,stats] = regress(y,X)
-[p,table,stats]       = anova1(y,x)
+x   = [1*ones(10,1);2*ones(10,1);3*ones(10,1)]; % factor variable for the strategies
+X   = [ones(30,1),x];                           % regression variable matrix
+[b,bint,r,rint,stats] = regress(y,X)            % regression and summary
+[p,table,stats]       = anova1(y,x)             % anova table
 
 set(gca,'LineWidth',1.6,'FontSize',16,'FontWeight','Bold');
