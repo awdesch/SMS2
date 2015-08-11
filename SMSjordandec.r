857d0ba1ef902e89c28db168d7d0617f8c1bc828
awdesch <melzeraw@hu-berlin.de>
2015-08-11 14:43:39 +0200

    Update SMSjordandec.r
    Keywords, description

--- SMSjordandec.r
+++ SMSjordandec.r
@@ -4,12 +4,12 @@
 #  Quantlet:    SMSjordandec
 # ----------------------------------------------------------------------------
 #  Description: SMSjordandec performs a Jordan decomposition of a symmetric 
-#               (3 x 3) matrix
+#               (3 x 3) matrix using its eigenvalues and eigenvectors.
 # ----------------------------------------------------------------------------
 #  Usage:       -
 # ----------------------------------------------------------------------------
-#  Keywords:    spectral decomposition, Jordan decomposition, eigenvalue,
-#               eigenvector
+#  Keywords:    spectral decomposition, eigenvalues, eigenvectors,
+#               decomposition, eigenvalue decomposition
 # ----------------------------------------------------------------------------
 #  See also:    SMSellipse, SMSjordandec
 # ----------------------------------------------------------------------------
