0983d1cb52f987640316b876a2ad86fc8bbc4a1d
awdesch <melzeraw@hu-berlin.de>
2015-08-11 14:35:25 +0200

    Create SMSjordandec.r

---
+++ SMSjordandec.r
@@ -0,0 +1,31 @@
+# ----------------------------------------------------------------------------
+#  Book:        Multivariate Statistics: Exercises and Solutions Series 
+# ----------------------------------------------------------------------------
+#  Quantlet:    SMSjordandec
+# ----------------------------------------------------------------------------
+#  Description: SMSjordandec performs a Jordan decomposition of a symmetric 
+#               (3 x 3) matrix
+# ----------------------------------------------------------------------------
+#  Usage:       -
+# ----------------------------------------------------------------------------
+#  Keywords:    spectral decomposition, Jordan decomposition, eigenvalue,
+#               eigenvector
+# ----------------------------------------------------------------------------
+#  See also:    SMSellipse, SMSjordandec
+# ----------------------------------------------------------------------------
+#  Author:      Dana Chromikova
+# ----------------------------------------------------------------------------
+
+
+# clear variables and close windows
+rm(list=ls(all=TRUE))
+graphics.off()
+
+# main calculation
+A      = cbind(c(1,2,3),c(2,1,2),c(3,2,1))   # matrix A
+u      = eigen(A)                            # eigenvalues and eigenvectors of A
+gama   = u$vec                               # matrix of eigenvectors
+gama
+lambda = diag(u$val)                         # diagonal matrix containing the eigenvalues
+lambda
+gama%*%lambda%*%t(gama)                      # Jordan decomposition
