fbf012edf7f143bcd050e48a1ee069a4f74ffcf7
awdesch <melzeraw@hu-berlin.de>
2015-08-13 09:50:18 +0200

    looks, header, keywords, description

--- SMScovbank.r
+++ SMScovbank.r
@@ -3,20 +3,24 @@
 # ---------------------------------------------------------------------
 # Quantlet:     SMScovbank 
 # ---------------------------------------------------------------------
-# Description:  SMScovbank calculates the covariance matrix
-#               (and eigevalues) of the bank2 dataset (bank2.rda)
+# Description:  It calculates the covariance matrix
+#               (and eigevalues) of the counterfeit bank notes
+#               (observations 101-200) from the bank2 dataset (bank2.rda)
+#               after they were lineary transformed by a vector
+#               a = (1, 1, 1, 1, 1, 1)'.
 # ---------------------------------------------------------------------
 # Usage:        -
 # ---------------------------------------------------------------------
 # See also:     SMSanovapull, SMScovbank, SMSdete2pull, SMSdeterpull,
 #               SMSlinregpull, SMSscabank45
 # ---------------------------------------------------------------------
-# Keywords:     covariance, eigenvalues, spectral decomposition
+# Keywords:     covariance, eigenvalues, spectral decomposition,
+#               multivariate, multi dimensional, variance, transformation
 # ---------------------------------------------------------------------
 # Inputs:       None
 # ---------------------------------------------------------------------
-# Output:       covariance matrix (and eigevalues) of the bank2 dataset
-#               (bank2.rda)
+# Output:       covariance matrix (and eigevalues) of the transformed
+#               counterfeit bank notes from bank2 dataset (bank2.rda)
 # ---------------------------------------------------------------------
 # Example:      -
 # ---------------------------------------------------------------------
@@ -24,18 +28,17 @@
 # ---------------------------------------------------------------------
 
 # clear variables and close windows
-# rm(list=ls(all=TRUE))
-# graphics.off()
+ rm(list=ls(all=TRUE))
+ graphics.off()
 
-# set working directory
-# setwd("C:/...")
+# setwd("C:/...")                         # set working directory
 
-library(SMSdata)
-data(bank2)
-V = var(bank2)					#variance matrix
+load("bank2.rda")                         # load data
+
+V = var(bank2)                            # variance matrix
 V
-e = eigen(V)$val					#eigenvalues of V
+e = eigen(V)$val                          # eigenvalues of V
 e
 a = rep(1,times=6)
-v = var(as.matrix(bank2[101:200,])%*%a)		#the variance of the sum
+v = var(as.matrix(bank2[101:200,])%*%a)   # the variance of the sum
 v
