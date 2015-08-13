e39da63a03dcaf76d686857ccccdbf623714a2f1
awdesch <melzeraw@hu-berlin.de>
2015-08-13 10:00:33 +0200

    last changes header

--- SMScovbank.r
+++ SMScovbank.r
@@ -22,7 +22,26 @@
 # Output:       covariance matrix (and eigevalues) of the transformed
 #               counterfeit bank notes from bank2 dataset (bank2.rda)
 # ---------------------------------------------------------------------
-# Example:      -
+# Example:
+#
+# Covariance matrix:
+# round(cov(bank2),4)
+#                    Length Height Left Height Right Inner Frame Lower Inner Frame Upper Diagonal
+# Length             0.1418      0.0314       0.0231           -0.1032           -0.0185   0.0843
+# Height Left        0.0314      0.1303       0.1084            0.2158            0.1050  -0.2093
+# Height Right       0.0231      0.1084       0.1633            0.2841            0.1300  -0.2405
+# Inner Frame Lower -0.1032      0.2158       0.2841            2.0869            0.1645  -1.0370
+# Inner Frame Upper -0.0185      0.1050       0.1300            0.1645            0.6447  -0.5496
+# Diagonal           0.0843     -0.2093      -0.2405           -1.0370           -0.5496   1.3277
+#
+# Eigenvalues:
+# e
+# 3.00030487 0.93562052 0.24341371 0.19465874 0.08521185 0.03551468
+#
+# Variance of the transformed (summed over rows) counterfeit bank notes
+# v
+# 1.742282
+#
 # ---------------------------------------------------------------------
 # Author:       Dana Chromikova
 # ---------------------------------------------------------------------
@@ -40,5 +59,5 @@ V
 e = eigen(V)$val                          # eigenvalues of V
 e
 a = rep(1,times=6)
-v = var(as.matrix(bank2[101:200,])%*%a)   # the variance of the sum
+v = var(as.matrix(bank2[101:200,])%*%a)   # the variance of the row sums
 v
