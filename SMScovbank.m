c75175c14ee4a0888882bbd4b247cf56c8a1339b
awdesch <melzeraw@hu-berlin.de>
2015-08-13 10:09:40 +0200

    update header, comments, description, keywords

--- SMScovbank.m
+++ SMScovbank.m
@@ -3,14 +3,18 @@
 % -------------------------------------------------------------------------
 % Quantlet:    SMScovbank
 % -------------------------------------------------------------------------
-% Description: SMScovbank calculate the covariance matrix (and eigevalues)
-%              of the Swiss Bank (bank2.dat) dataset.
+% Description: It calculates the covariance matrix (and eigevalues)
+%              of the Swiss Bank (bank2.dat) dataset and the variance of
+%              the counterfeit bank notes (observations 101-200) after they 
+%              were lineary transformed by a vector a = (1, 1, 1, 1, 1, 1)'.
 % -------------------------------------------------------------------------
 % Input:       None.
 % -------------------------------------------------------------------------
 % Output:      Eigenvalues and covariance matrix for the bank2 dataset.
 % -------------------------------------------------------------------------
 % Results:    
+%
+%   Covariance matrix:
 %   V =
 %
 %    0.1418    0.0314    0.0231   -0.1032   -0.0185    0.0843
@@ -20,6 +24,7 @@
 %   -0.0185    0.1050    0.1300    0.1645    0.6447   -0.5496
 %    0.0843   -0.2093   -0.2405   -1.0370   -0.5496    1.3277
 %
+%   Eigenvalues:
 %   e =
 %
 %    3.0003         0         0         0         0         0
@@ -29,12 +34,14 @@
 %         0         0         0         0    0.0852         0
 %         0         0         0         0         0    0.0355%
 %
-%
+%   Variance of the transformed counterfeit bank notes:
 %   v =
 %
 %    1.7423
 % -------------------------------------------------------------------------
-% Keywords:    Covariance, eigen values
+% Keywords:    covariance, eigenvalues, spectral decomposition,
+%              decomposition, eigenvalue decomposition, multivariate,
+%              multi dimensional, variance, transformation
 % -------------------------------------------------------------------------
 % See also:    SMSanovapull, SMSdete2pull, SMSdeterpull, SMSlinregpull,
 %              SMSscabank45
@@ -48,10 +55,10 @@ clc
 
 load bank2.dat;
 
-V = cov(bank2);                  %variance matrix
+V      = cov(bank2);                  % variance matrix
 V
-[v, e] = eigs(V);                %eigenvalues of V
+[v, e] = eigs(V);                     % eigenvalues of V
 e
-a = ones(6,1);
-v = var((bank2(101:200,:))*a);   %the variance of the sum
+a      = ones(6,1);
+v      = var((bank2(101:200,:))*a);   % the variance of the row sum
 v
