8ffca11aebca3c92b6de30ebe72663f3dd7a09f0
Awdesch Melzer <melzeraw@cms.hu-berlin.de>
2015-11-30 09:50:41 +0100

    changes due to print

--- SMScluscrime/SMScluscrime.R
+++ SMScluscrime/SMScluscrime.R
@@ -6,26 +6,39 @@ rm(list=ls(all=TRUE))
  
 load("uscrime.rda")
 
-op  = par(mfrow=c(1,2))
-lab = paste(row.names(uscrime),uscrime$reg)
+op   = par(mfrow=c(1,2))
+lab  = paste(row.names(uscrime),as.numeric(uscrime$reg))
 row.names(uscrime)=lab
-x   = uscrime[,3:9]
-x   = t(t(x)-apply(x,2,mean))
-x   = as.matrix(x)%*%diag(1/(apply(x,2,sd)))
-hc  = hclust(dist(x),"ward")
-cl  = cutree(hc,4)
+x    = uscrime[,3:9]
+x    = t(t(x)-apply(x,2,mean))
+x    = as.matrix(x)%*%diag(1/(apply(x,2,sd)))
+hc   = hclust(dist(x),"ward.D")
+cl   = cutree(hc,4)
 names(cl)
-plot(hc,labels=lab,main="Dendogram for US crime, Ward algorithm",xlab="",sub="")
-pr  = prcomp(x)
+opar = par(mar=c(2, 5, 4, 2) +  0.1)
+plot(hc,labels=lab,main="Ward dendrogram for US crime",xlab="",sub="",cex=0.7,ylab="Euclidean distance")
+pr   = prcomp(x)
 plot(pr$x[,1:2],type="n",main="US crime")
 text(pr$x[,1:2],lab,col=as.numeric(cl)+1)
-par(op)
+#par(op)
+
+## Cluster 1
+substr(lab[cl==1],1,5)
+# Cluster 2
+substr(lab[cl==2],1,5)
+# cluster 3
+substr(lab[cl==3],1,5)
+# cluster 4
+substr(lab[cl==4],1,5)
 
 ## x is dataframe
 
-x   = data.frame(x)
+x     = data.frame(x)
 colnames(x)=colnames(uscrime)[3:9]
 
-## table of means in four clusters
+# clusters and regions
+table(uscrime$reg,cl)
 
+## table of means in four clusters
 sapply(x,tapply,cl,mean)
+cl.eucl= cl
